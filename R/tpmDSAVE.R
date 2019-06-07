79194123a6dc7d1f8d55aa109b6fafedaefbe0fe
Johan Gustafsson <gustajo@chalmers.se>
2019-06-07 22:29:32 +0200

    fix: Optimized the TPM function slightly

--- R/tpmDSAVE.R
+++ R/tpmDSAVE.R
@@ -6,19 +6,11 @@
 #'
 #' @param counts A matrix.
 #' @return A matrix
-#' @author Juan Inda, <inda@@chalmers.se>
+#' @author Juan Inda, <inda@@chalmers.se>, Johan Gustafsson, <gustajo@@chalmers.se>
 tpmDSAVE <- function(counts){
-  counts2 <- counts
   for(i in 1:dim(counts)[2]){
-    #s <- seq(from=1, to=dim(counts)[2], by = round(dim(counts)[2]/10))
-    #if(i %in% s) print(paste("Doing: ", round(i/dim(counts)[2] * 100), "%"))
-    nc <- counts[,i] / sum(counts[,i])
-    nc[is.na(nc)] <- 0
-    counts2[,i] <- nc * 1e6
+    counts[,i] <- counts[,i] * 1e6 / sum(counts[,i])
   }
-  return(counts2)
-  #if(dim(counts)[2] > 1000)
-  #tmp.tpm <- t(t(counts)/colSums(counts)) * 1e6
-  #tmp.tpm[is.na(tmp.tpm)] <- 0
-  #return(tmp.tpm)
+  counts[is.na(counts)] <- 0
+  return(counts)
 }
