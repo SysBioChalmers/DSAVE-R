456529bf49881d508c4e478f7454d5b56f8eac7f
Johan Gustafsson <gustajo@chalmers.se>
2019-11-28 21:43:40 +0100

    fix: Updated bulk variables and variation scores for datasets according to the latest changes in DSAVE-M

--- R/createPackageVariables.R
+++ R/createPackageVariables.R
@@ -14,15 +14,15 @@ createPackageVariables <- function() {
 
 #public dataset scores
 #The numbers come from the publication, figure 3A.
-datasetScoresHuman <- list(list("BC", 0.1378),
-                           list("OC", 0.0389),
-                           list("LC", 0.0283),
-                           list("LIVC", 0.1102),
-                           list("PBMC68k", 0.0352),
-                           list("B10k", 0.0194),
-                           list("CD4TMEM", 0.0139),
-                           list("HCA CB", 0.0163),
-                           list("CD8T", 0.0321))
+datasetScoresHuman <- list(list("BC", 0.1371),
+                           list("OC", 0.0385),
+                           list("LC", 0.0270),
+                           list("LIVC", 0.1096),
+                           list("PBMC68k", 0.0351),
+                           list("B10k", 0.0190),
+                           list("CD4TMEM", 0.0137),
+                           list("HCA CB", 0.0160),
+                           list("CD8T", 0.0326))
 
 
 usethis::use_data(datasetScoresHuman, overwrite = T)
@@ -30,13 +30,7 @@ usethis::use_data(datasetScoresHuman, overwrite = T)
 
 #bulk variation
 #This requires access to the data, which is not provided in the package
-samp  <- as.matrix(read.table("data/tcellCD4ProfilesTMMNormalized.txt", header = T, sep = "\t", row.names = 1))
-samp <- samp[,32:39]
-
-#scale so the average count per sample is 10^6
-normFact = 10^6 / mean(colSums(samp))
-samp = samp * normFact
-
+samp  <- as.matrix(read.table("data/scaledTMMMatrix.txt", header = T, sep = "\t", row.names = 1))
 
 
 bulkMean1Vs1_05_100k <- DSAVEGetTotalVariationFromBulk(samp,
@@ -86,6 +80,7 @@ bulkTotalVar4vs4 <- list(list("PseudoTPM: 0.5-100k", bulkMean4Vs4_05_100k),
                           list("PseudoTPM: 0.5-50", bulkMean4Vs4_05_50),
                           list("PseudoTPM: 2-100", bulkMean4Vs4_2_100))
 
+#bulkTotalVar1vs1[[1]][[2]] = bulkTotalVar1vs1[[1]][[2]]*5
 usethis::use_data(bulkTotalVar1vs1, overwrite = T)
 usethis::use_data(bulkTotalVar4vs4, overwrite = T)
 
