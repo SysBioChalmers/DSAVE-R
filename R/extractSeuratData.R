09d081392e419134d152ee9ae7e3105b02587d80
Johan Gustafsson <gustajo@chalmers.se>
2019-12-16 16:04:26 +0100

    feat: added function for extraction of data from Seurat

---
+++ R/extractSeuratData.R
@@ -0,0 +1,23 @@
+#' downloadData
+#'
+#' Extracts the data from a Seurat object. Optionally, a cluster id can be sent in, to extract
+#' a subset of the data
+#'
+#' @importFrom graphics hist
+#' @importFrom Seurat Read10X
+#' @importFrom utils untar download.file
+#' @export
+#' @author Johan Gustafsson, <gustajo@@chalmers.se>
+#' @return the template
+
+extractSeuratData <- function(seuratObj, clusterId = NA) {
+
+  if (is.na(clusterId)) {
+    data = so@assays$RNA@counts
+  } else {
+    data = so@assays$RNA@counts[,so@active.ident == clusterId]
+  }
+
+  return (data)
+}
+
